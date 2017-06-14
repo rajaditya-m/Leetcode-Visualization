a64f136f31b69d02105d867940e19aebf9d0874d
rajaditya-m <rajadityamukherjee.osu@outlook.com>
2017-06-14 14:36:36 -0400

    Introduction

---
+++ analytics.py
@@ -0,0 +1,143 @@
+import os
+
+DIFFICULTYMAP = {}
+COMPANYWISEPROBLEMSETLIST = []
+COMPANYWISEPROBLEMLISTLIST = []
+SOLVEDPROBLEMSET = set()
+SOLVEDPROBLEMLIST = []
+HITCOUNTER = {}
+SORTEDHITS = []
+
+def readSolvedProblems(filename):
+	f = open(filename)
+	for line in f:
+		probId = int(line)
+		SOLVEDPROBLEMLIST.append(probId)
+		SOLVEDPROBLEMSET.add(probId)
+	f.close()
+	SOLVEDPROBLEMLIST.sort()
+
+def readAllCompanyData(filenames):
+	for filepaths in filenames:
+		companyName, problemList, problemSet = readData(filepaths)
+		COMPANYWISEPROBLEMSETLIST.append(problemSet)
+		COMPANYWISEPROBLEMLISTLIST.append(problemList)
+	for key, value in HITCOUNTER.items():
+		SORTEDHITS.append((value, key))
+	SORTEDHITS.sort(key=lambda tup: tup[0], reverse=True )
+
+
+
+def difficultyStringtoId(str):
+	str = str.strip()
+	if str=='Easy':
+		return 0
+	elif str == 'Medium':
+		return 1
+	else:
+		return 2
+
+def classifyProblemListByDifficulty(probIds):
+	easy = []
+	medium = []
+	hard = []
+	for probs in probIds:
+		if DIFFICULTYMAP[probs]==1:
+			medium.append(probs)
+		elif DIFFICULTYMAP[probs]==2:
+			hard.append(probs)
+		else:
+			easy.append(probs)
+	return [easy, medium, hard]
+
+def classifyProblemListBySolved(probIds):
+	solved = []
+	unsolved = []
+	for probs in probIds:
+		if probs in SOLVEDPROBLEMSET:
+			solved.append(probs)
+		else:
+			unsolved.append(probs)
+	return [solved, unsolved]
+
+def getTopKUnsolved(k):
+	counter = 0
+	topKUnsolved = []
+	for items in SORTEDHITS:
+		if items[1] in SOLVEDPROBLEMSET:
+			continue
+		topKUnsolved.append(items[1])
+		counter += 1
+		if (counter==k):
+			break
+	topKUnsolved.sort()
+	return topKUnsolved
+
+
+
+def analytics():
+	
+	#most frequently occuring problems
+	topkUnsolved =  getTopKUnsolved(40) #list(set.intersection(*COMPANYWISEPROBLEMSETLIST))
+	[tEasy, tMedium, tHard] = classifyProblemListByDifficulty(topkUnsolved)
+	print('\nMOST FREQUENTLY OCCURING UNSOLVED PROBLEMS')
+	print('*************EASY*******************************')
+	print([ (t, HITCOUNTER[t]) for t in tEasy])
+	print('*************MEDIUM*****************************')
+	print([ (t, HITCOUNTER[t]) for t in tMedium])
+	print('*************HARD*******************************')
+	print([ (t, HITCOUNTER[t]) for t in tHard])
+
+	#next find the union of all problems 
+	unionList = list(set.union(*COMPANYWISEPROBLEMSETLIST))
+	unionList.sort()
+	[uEasy, uMedium, uHard] = classifyProblemListByDifficulty(unionList)
+	print('\nALL PROBLEMS (UNION OF ALL COMPANIES)')
+	print('*************EASY*******************************')
+	# print(uEasy)
+	[ueSolved, ueUnsolved] = classifyProblemListBySolved(uEasy)
+	print('Solved({}/{}) {}'.format(len(ueSolved), len(uEasy), ueSolved))
+	print('Unsolved({}/{}) {}'.format(len(ueUnsolved), len(uEasy), ueUnsolved))
+	print('*************MEDIUM*****************************')
+	# print(uMedium)
+	[umSolved, umUnsolved] = classifyProblemListBySolved(uMedium)
+	print('Solved({}/{}) {}'.format(len(umSolved), len(uMedium), umSolved))
+	print('Unsolved({}/{}) {}'.format(len(umUnsolved), len(uMedium), umUnsolved))
+	print('*************HARD*******************************')
+	# print(uHard)
+	[uhSolved, uhUnsolved] = classifyProblemListBySolved(uHard)
+	print('Solved({}/{}) {}'.format(len(uhSolved), len(uHard), uhSolved))
+	print('Unsolved({}/{}) {}'.format(len(uhUnsolved), len(uHard), uhUnsolved))
+	
+
+
+def readData(filename):
+	#try to obtain the filename 
+	basename = os.path.basename(filename)
+	#remove the .txt portion 
+	dotIdx = basename.find('.')
+	companyName = basename[0:dotIdx].upper()
+	f = open(filename, 'r')
+	problemList = []
+	problemSet = set()
+	for line in f:
+		data = line.split(' ')
+		problemID = int(data[0])
+		difficultyString = data[-1]
+		if problemID not in DIFFICULTYMAP:
+			DIFFICULTYMAP[problemID] = difficultyStringtoId(difficultyString)
+		HITCOUNTER[problemID] = HITCOUNTER.get(problemID,0) + 1
+		problemList.append(problemID)
+		problemSet.add(problemID)
+	f.close()
+	print('Finished processing company {}'.format(companyName))
+	return [companyName, problemList, problemSet]
+
+CURRENTPATH = os.getcwd()
+DATADIRECTORYPATH = CURRENTPATH + '\data'
+COMPANYFILES = [os.path.join(DATADIRECTORYPATH,f) for f in os.listdir(DATADIRECTORYPATH) if os.path.isfile(os.path.join(DATADIRECTORYPATH,f)) ]
+SOLVEDPROBLEMFILE = CURRENTPATH + '\solved.txt'
+
+readAllCompanyData(COMPANYFILES)
+readSolvedProblems(SOLVEDPROBLEMFILE)
+analytics()
